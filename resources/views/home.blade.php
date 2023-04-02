53622fbe56b2353ace0e27235945ceeda96e3c33
anand54 <anandaaryal54@gmail.com>
2023-04-02 22:22:13 +0545

    commit 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,25 @@
 @include('component/header')
-<style>
-
-</style>
 <div class="mainContainer">
     <img src="{{ asset('images/banner.png') }}" alt="" class="homeBanner">
     <div class="text-block">
-        <h4><span class="spyra">SPYRA</span> <span class="solution">SOLUTION</span></h4>
-        <span class="for">FOR : </span> &nbsp; <span id="typewriter">App Development</span>
+        <h1><span class="spyra">SPYRA</span> <span class="solution">SOLUTION</span></h1>
+        <span class="for">FOR : </span> &nbsp; <span id="typewriter" class="typewriter"></span>
+        <div>
+            <a href="" class="socialMedia"><i class="bi bi-facebook"></i></a>
+            <a href="" class="socialMedia"><i class="bi bi-twitter"></i></a>
+            <a href="" class="socialMedia insta"><i class="bi bi-instagram"></i></a>
+            <a href="" class="socialMedia"><i class="bi bi-linkedin"></i></a>
+        </div>
       </div>
 </div>
-
-<script>
-$('#typewriter').typewriter({
-    text : ["String 1","String 2","String 3"],
-    typeDelay: 200,
-    waitingTime: 1000,
-});
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/1.1.1/typed.min.js"></script>
+<script type="text/javascript">
+$(function(){
+        $(".typewriter").typed({
+            strings: ["WEB APPLICATION DESIGN", "WEB APPLICATION DEVELOPMENT", "MOBILE APPLICATION DESIGN", "MOBILE APPLICATION DEVELOPMENT"],
+            typeSpeed: 10,
+            loop: true
+        });
+    });
 </script>
 @include('component/footer')
