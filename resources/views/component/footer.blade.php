e784c3936841126b66822ea6f1df9b34f38a7c09
anand54 <anandaaryal54@gmail.com>
2023-04-11 08:48:56 +0545

    commit 1 4/11/2023

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,3 +1,64 @@
+<div class="footer">
+    <div class="container">     
+        <div class="row">                       
+            <div class="col-lg-4 col-sm-4 col-xs-12">
+                <div class="single_footer">
+                    <h4><img src="{{ asset('images/logo/full-logo.png') }}" alt="" class="w-25"></h4>
+                    <ul>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                    </ul>
+                </div>
+            </div><!--- END COL --> 
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="single_footer single_footer_address">
+                    <h4>Page Link</h4>
+                    <ul>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc </a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                        <li><a class="aclass" href="#">Abc</a></li>
+                    </ul>
+                </div>
+            </div><!--- END COL -->
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="single_footer single_footer_address">
+                    <h4>Subscribe today</h4>
+                    <div class="signup_form">                           
+                        <form action="#" class="subscribe">
+                            <input type="text" class="subscribe__input" placeholder="Enter Email Address">
+                            <button type="button" class="subscribe__btn"><i class="fas fa-paper-plane"></i></button>
+                        </form>
+                    </div>
+                </div>
+                <div class="social_profile">
+                    <ul>
+                        <li><a class="aclass" href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a class="aclass" href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a class="aclass" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                        <li><a class="aclass" href="#"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>                          
+            </div><!--- END COL -->         
+        </div><!--- END ROW --> 
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 col-xs-12">
+              <p class="copyright"> 
+                <span>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-twitter"></i></a>
+              </span> Copyright © 2023 <a class="aclass" href="#">SPYRA SOLUTION</a>.</p>
+            </div><!--- END COL -->                 
+        </div><!--- END ROW -->                 
+    </div><!--- END CONTAINER -->
+</div>
+
 <script type="text/javascript">
     $(function(){
             $(".typewriter").typed({
--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <title>SPYRA</title>
 </head>
 <body>
